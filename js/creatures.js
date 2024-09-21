d99beb8c9a7d82863a2e419854a0bc3dbbda4889
tibiantis-notes <tibiantis.notes@gmail.com>
2024-09-21 11:49:35 +0100

    Update creatures.js

--- js/creatures.js
+++ js/creatures.js
@@ -81,4 +81,4 @@ const creature = [behemoth];
   document.getElementById('creature_log').innerHTML += "The " + animal.name + " weighs " + animal.weight + "kg<br>";
 }); */
 
-document.getElementById('creature_log').innerHTML = "behemoth hp2";
+document.getElementById('creature_log').innerHTML = "behemottrytryeh hp2";
