8e48c6695357d5d6966219d05949ac9640fece5a
tibiantis-notes <tibiantis.notes@gmail.com>
2024-09-21 12:00:19 +0100

    Update creatures.js

--- js/creatures.js
+++ js/creatures.js
@@ -81,4 +81,4 @@ const creature = [behemoth];
   document.getElementById('creature_log').innerHTML += "The " + animal.name + " weighs " + animal.weight + "kg<br>";
 }); */
 
-document.getElementById('creature_log').innerHTML = "behemoth.hp = " + behemoth.hp;
+document.getElementById('creature_log').innerHTML = "behemoth.hp = " + behemoth.attack[0].damage_base;
