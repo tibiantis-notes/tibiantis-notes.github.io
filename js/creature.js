d39c5af97bc37d0ad6c891bef762da3532b1383b
tibiantis-notes <tibiantis.notes@gmail.com>
2024-10-30 23:10:14 +0000

    Update creature.js

--- js/creature.js
+++ js/creature.js
@@ -60,7 +60,7 @@ creature.forEach(current_creature => {
 //build columns based on array
 // or lots or conditionals
 // maybe only double the conditionals.
-let header = false;
+let header; // undefined = false
 if (header == false){
     creatureTable += "<tr><th>Name</th><th>HitPoints</th></tr>";
     header = true;
