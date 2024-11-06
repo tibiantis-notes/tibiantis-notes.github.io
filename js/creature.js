d1bf11e8a51bbc208fcdaf4278811108109b050a
tibiantis-notes <tibiantis.notes@gmail.com>
2024-11-06 21:42:52 +0000

    Update creature.js

--- js/creature.js
+++ js/creature.js
@@ -36,7 +36,7 @@ function resistances(fn_creature){
 //Fire F, Poison P, Energy E, Physical Ph, Life Drain, LD
 let resist_text = "";
  if (fn_creature.NoFire == true){
- resist_text="<img src="/images/fire.png" alt="fire" style="width:32px;height:32px;">"   
+ resist_text='<img src="/images/fire.png" alt="fire" style="width:32px;height:32px;">'   
 }
 return resist_text;
 
