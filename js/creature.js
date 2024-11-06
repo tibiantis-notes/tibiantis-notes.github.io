8712bc824ea4189160a2964480574e804001e13c
tibiantis-notes <tibiantis.notes@gmail.com>
2024-11-06 21:56:49 +0000

    Update creature.js

--- js/creature.js
+++ js/creature.js
@@ -35,10 +35,9 @@ function resistances(fn_creature){
 
 //Fire F, Poison P, Energy E, Physical Ph, Life Drain, LD
 let resist_text = "";
- /*if (fn_creature.NoBurning == true){
- //resist_text='<img src="/images/fire.png" alt="fire" style="width:32px;height:32px;">'   
-resist_text= "fire";
-}*/
+ if (fn_creature.NoBurning == true){
+ resist_text='<img src="/images/fire.png" alt="fire" style="width:32px;height:32px;">'   
+}
 
 
 return resist_text;
